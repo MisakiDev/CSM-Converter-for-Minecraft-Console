9d2bb70bf5b91836db9a9820e966a876c137fe5c
Misaki Sagiri <59036402+MisakiDev@users.noreply.github.com>
2021-01-19 16:21:46 +0100

    Add files via upload

---
+++ CSM Converter/Settings.Designer.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.CodeDom.Compiler;
+using System.Configuration;
+using System.Runtime.CompilerServices;
+
+namespace CSM_Converter.Properties
+{
+	// Token: 0x02000010 RID: 16
+	[CompilerGenerated]
+	[GeneratedCode("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "11.0.0.0")]
+	internal sealed partial class Settings : ApplicationSettingsBase
+	{
+		// Token: 0x17000004 RID: 4
+		// (get) Token: 0x06000053 RID: 83 RVA: 0x000151F8 File Offset: 0x000133F8
+		public static Settings Default
+		{
+			get
+			{
+				return Settings.settings_0;
+			}
+		}
+
+		// Token: 0x04000056 RID: 86
+		private static Settings settings_0 = (Settings)SettingsBase.Synchronized(new Settings());
+	}
+}
