6398cbfe7d2736ce7b14b57ce157b40a7a818889
Misaki Sagiri <59036402+MisakiDev@users.noreply.github.com>
2021-01-19 16:22:37 +0100

    Add files via upload

---
+++ CSM Converter/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+using System.Runtime.Versioning;
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("CSM Converter")]
+[assembly: AssemblyCompany("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: Guid("3f652e40-0b41-4f1e-bbb0-2f77ec97d98d")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyProduct("CSM Converter")]
